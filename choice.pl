026309919ebc58148fd8158c5a62ff7aedfa55fc
Kanoe Hiromi <kyou.wk@gmail.com>
2012-01-04 21:08:08 +0800

    Updated

--- choice.pl
+++ choice.pl
@@ -9,37 +9,39 @@ $VERSION = "0.1";
     contact     => "hiromi\@neesan.org",
     name        => "Choice script",
     description => "Choice bot",
-    license     => "Free",
     url         => "http:\/\/fun-at.the-circ.us",
-    changed     => "nil"
+    changed     => "040112"
 );
 #Usage !choose option1, option 2, option 3, optionN
 use Irssi::Irc;
 use strict;
 
 
-#"message public", SERVER_REC, char *msg, char *nick, char *address, char *target
+#Only listen on channels listed below. Add more channels below
+my @channels = ('#channel1', '#channel2', '#channel3');
 sub choice{
+#"message public", SERVER_REC, char *msg, char *nick, char *address, char *target
         my($server, $msg, $nick, $address, $target) = @_;
 
 	#change trigger below
+  if(grep(/^$target$/, @channels)){
         if($msg =~ /^!choose/ ){
                 $msg =~ s/!choose //;
                 
-
+                #arguments passed in as one whole string, separate them by commas and store in array
                 my @choices = split(',', $msg);
                 my $range = @choices;
-                my @fch;
+                my @fch; #final choices
+
                 #clean up extra space
                 foreach my $ch(@choices){
                         $ch =~ s/^ //;
                         push(@fch, $ch);
                 }
-
+                #randomly choose a choice
                 my $random = int(rand($range)) + 0;
-		#Only listen on channels listed below. Add more channels below
-                if($target eq '#channel' || $target eq '#channel1' || $target eq '#channelN'){
-                        $server->command('msg '.$target.' '.$nick.' : '.$fch[$random].'!');
+                #output
+                $server->command('msg '.$target.' '.$nick.' : '.$fch[$random].'!');
                 }
                 }
 }
